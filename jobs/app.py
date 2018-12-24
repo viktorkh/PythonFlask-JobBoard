4cb22a97f87f38235bcdfbc26d3715a60c1dae3d
Victor Khazanov <victor.khazanov@clicksoftware.com>
2018-12-24 13:06:07 +0200

    commit module7

--- jobs/app.py
+++ jobs/app.py
@@ -1,5 +1,5 @@
 import sqlite3 
-from flask import Flask, render_template, g
+from flask import Flask, render_template, g, request, redirect, url_for
 
 PATH='db/jobs.sqlite'
 
@@ -42,6 +42,7 @@ def job(job_id):
     job = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id WHERE job.id = ?',[job_id],single=True)
     return render_template('job.html',job=job)
 
+
 @app.route('/employer/<employer_id>')
 def employer(employer_id):
     employer = execute_sql('SELECT * FROM employer WHERE id=?',[employer_id],single=True)
@@ -49,3 +50,18 @@ def employer(employer_id):
     reviews = execute_sql('SELECT review, rating, title, date, status FROM review JOIN employer ON employer.id = review.employer_id WHERE employer.id = ?',[employer_id])
     return render_template('employer.html',employer=employer,jobs=jobs,reviews=reviews)
 
+
+@app.route('/employer/<employer_id>/review', methods=('GET','POST'))
+def review(employer_id):
+    if request.method == 'POST'
+        review = request.form['review']
+        rating = request.form['rating']
+        title = request.form['title']
+        status = request.form['status']
+        
+        date = datetime.datetime.now().strftime("%m/%d/%Y")
+        execute_sql('INSERT INTO review (review, rating, title, date, status, employer_id) VALUES (?, ?, ?, ?, ?, ?)',review, rating, title, date, status, employer_id),commit=True)
+
+        return redirect(url_for('employer',employer_id=employer_id))
+
+    return render_template('review.html',employer_id=employer_id)
