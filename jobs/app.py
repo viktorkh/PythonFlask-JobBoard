91456de18ec6cf820b07b96bec7523c2d26b055f
Victor Khazanov <victor.khazanov@clicksoftware.com>
2018-12-24 13:12:57 +0200

    m7v3

--- jobs/app.py
+++ jobs/app.py
@@ -8,7 +8,7 @@ app= Flask(__name__)
 
 
 def open_connection():
-    connection = getattr(g,'_connection', None)
+    connection = getattr(g,'_connection', None) 
     if connection == None:
         connection = g._connection =sqlite3.connect(PATH)
     connection.row_factory = sqlite3.Row
